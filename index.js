e4253324e991fbcf4bd788c74dba264f0c3e2023
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 16:52:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = []
-  for (var i = 0; i < 5; i++) {
-    array(0)
+  for (var i = 0; i < musicians.length; i++) {
+   ` musicians[i]`
   }
 }
