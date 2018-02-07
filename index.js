9fe825061424fdd422154464e19b091935ab4dd2
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 17:31:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return arr
 }
+function johnLennonFacts(facts){
+  var moreFacts = [];
+  var i = 0
+  while (i < facts.length){
+    moreFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return moreFacts;
+}
